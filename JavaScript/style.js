c6602b90a31c2e05c51891c5d77448442d9943d1
Boris Betremieux <b.boris.2001@gmail.com>
2023-10-26 17:58:20 +0200

    Minor changes

--- JavaScript/style.js
+++ JavaScript/style.js
@@ -7,26 +7,26 @@ for (let i = 0; i < buttons.length; i++)
     {
         if (e.button == 0) {
             button.style.backgroundColor = "#000";
-            button.style.backgroundImage = "linear-gradient(rgba(0,0,0,1), rgba(136,136,136,1))";
+            button.style.backgroundImage = "linear-gradient(#000, #888)";
             button.style.color = "#fff";
         }
     });
     button.addEventListener("click", () => 
     {
         button.style.backgroundColor = "#222";
-        button.style.backgroundImage = "linear-gradient(rgba(136,136,136,1), rgba(0,0,0,1))";
+        button.style.backgroundImage = "linear-gradient(#888, #000)";
         button.style.color = "#fff";
     });
     button.addEventListener("mouseleave", () => 
     {
         button.style.backgroundColor = "#eee";
-        button.style.backgroundImage = "linear-gradient(rgba(255,255,255,1), rgba(200,200,200,1))";
+        button.style.backgroundImage = "linear-gradient(#fff, #bbb)";
         button.style.color = "#000";
     });
     button.addEventListener("mouseover", () => 
     {
         button.style.backgroundColor = "#222";
-        button.style.backgroundImage = "linear-gradient(rgba(136,136,136,1), rgba(0,0,0,1))";
+        button.style.backgroundImage = "linear-gradient(#888, #000)";
         button.style.color = "#fff";
     });
 }
@@ -38,7 +38,7 @@ for (let i = 0; i < submitButtons.length; i++)
     {
         if (e.button == 0) {
             button.style.backgroundColor = "#000";
-            button.style.backgroundImage = "linear-gradient(rgba(0,0,0,1), rgba(136,136,136,0.5)), url(../Images/Laitue2.jpg)";
+            button.style.backgroundImage = "linear-gradient(#000, #88888880), url(../Images/Laitue2.jpg)";
             button.style.backgroundSize = "cover";
             button.style.color = "#fff";
         }
@@ -46,20 +46,20 @@ for (let i = 0; i < submitButtons.length; i++)
     button.addEventListener("click", () => 
     {
         button.style.backgroundColor = "#222";
-        button.style.backgroundImage = "linear-gradient(rgba(136,136,136,0.5), rgba(0,0,0,1)), url(../Images/Laitue2.jpg)";
+        button.style.backgroundImage = "linear-gradient(#88888880, #000), url(../Images/Laitue2.jpg)";
         button.style.backgroundSize = "cover";
         button.style.color = "#fff";
     });
     button.addEventListener("mouseleave", () => 
     {
         button.style.backgroundColor = "#eee";
-        button.style.backgroundImage = "linear-gradient(rgba(136,136,136,1), rgba(0,0,0,1))";
+        button.style.backgroundImage = "linear-gradient(#888, #000)";
         button.style.color = "#fff";
     });
     button.addEventListener("mouseover", () => 
     {
         button.style.backgroundColor = "#222";
-        button.style.backgroundImage = "linear-gradient(rgba(136,136,136,0.5), rgba(0,0,0,1)), url(../Images/Laitue2.jpg)";
+        button.style.backgroundImage = "linear-gradient(#88888880, #000), url(../Images/Laitue2.jpg)";
         button.style.backgroundSize = "cover";
         button.style.color = "#fff";
     });
