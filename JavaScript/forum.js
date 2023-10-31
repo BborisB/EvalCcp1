1dab29f7e55ddc8dc4f6fdf04470e6d79e9e3bfd
Boris Betremieux <b.boris.2001@gmail.com>
2023-10-31 17:39:53 +0100

    subject

--- JavaScript/forum.js
+++ JavaScript/forum.js
@@ -13,21 +13,21 @@ if(categories==null)
         title: "Commerce",
         description: "Découvrez et échangez les meilleures astuces de cuisine avec la laitue, et partagez vos meilleures recettes de laitue ici.",
         backgroundImage: "../Images/laitueCaisse.jpg",
-        subjects: [{title: "test", id: 1, auteur: "Boris", lastComment: "XX/XX/XXXX", messages: [{auteur: userTest, message: "Je ne vends pas de laitue, j'en mange.", date: "XX/XX/XXXX"}]}]
+        subjects: [{title: "Laitue", id: 1, auteur: "Boris", lastComment: "XX/XX/XXXX", messages: [{auteur: userTest, message: "Je ne vends pas de laitue, mais j'en mange.", date: "XX/XX/XXXX"}]}]
     };
     let cat2 = 
     {
         title: "Culture",
         description: "Découvrez et échangez les méthodes de culture de la laitue, et partagez avec les cultivateurs pour découvrir les secrets de la laitue.",
         backgroundImage: "../Images/laitueChamp.jpg",
-        subjects: [{title: "test", id: 1, auteur: "Boris", lastComment: "XX/XX/XXXX", messages: [{auteur: userTest, message: "Je ne cultive pas la laitue, je la mange.", date: "XX/XX/XXXX"}]}]
+        subjects: [{title: "Laitue", id: 1, auteur: "Boris", lastComment: "XX/XX/XXXX", messages: [{auteur: userTest, message: "Je ne cultive pas la laitue, mais je la mange.", date: "XX/XX/XXXX"}]}]
     };
     let cat3 = 
     {
         title: "Cuisine",
         description: "Découvrez et échangez avec les marchands, commercants et autres fans de laitue, et partagez vos meilleurs plans pour acheter de la laitue.",
         backgroundImage: "../Images/laitueCuisine.jpg",
-        subjects: [{title: "test", id: 1, auteur: "Boris", lastComment: "XX/XX/XXXX", messages: [{auteur: userTest, message: "Je ne cuisine pas la laitue, je la mange.", date: "XX/XX/XXXX"}]}]
+        subjects: [{title: "Laitue", id: 1, auteur: "Boris", lastComment: "XX/XX/XXXX", messages: [{auteur: userTest, message: "Je ne cuisine pas la laitue, mais je la mange.", date: "XX/XX/XXXX"}]}]
     };
     categories = [cat1, cat2, cat3];
     localStorage.setItem("categories", JSON.stringify(categories));
--- JavaScript/subject.js
+++ JavaScript/subject.js
@@ -8,95 +8,115 @@ else
     let currentCategory = categories[localStorage.getItem("currentCategoryIndex")];
     if(currentCategory==null)
     {
-        location.href = location.href.replace("subject.html", "categorie.html");
+        location.href = location.href.replace("subject.html", "forum.html");
     }
-    let chat = document.querySelector("#chat");
-    let textAreaMessage = document.querySelector("#textAreaMessage");
-    let sendBtn = document.querySelector("#sendBtn");
-
-    textAreaMessage.style.height="1em";
-        textAreaMessage.style.height = "calc("+textAreaMessage.scrollHeight+"px - 1.245em)";
-    textAreaMessage.addEventListener("input", ()=>
+    else
     {
-        textAreaMessage.style.height="1em";
-        textAreaMessage.style.height = "calc("+textAreaMessage.scrollHeight+"px - 1.245em)";
-        textAreaMessage.scrollTop = textAreaMessage.scrollHeight;
-    });
-
-    //Redimensionne le textArea quand il change de taille (surtout quand il change de largeur).
-    function textAreaResize()
-    {
-        textAreaMessage.style.height="1em";
-        textAreaMessage.style.height = "calc("+textAreaMessage.scrollHeight+"px - 1.245em)";
-    }
-    new ResizeObserver(textAreaResize).observe(textAreaMessage)
-       
-
-    textAreaMessage.addEventListener("keydown", (e)=>
-    {
-        if(e.key=="Enter" && !e.shiftKey)
+        let currentSubject = currentCategory.subjects[localStorage.getItem("currentSubjectId")-1];
+        if(currentSubject==null)
         {
-            e.preventDefault();
-            let text = textAreaMessage.value.trim();
-            if(text)
+            location.href = location.href.replace("subject.html", "category.html");
+        }
+        else
+        {
+            let chat = document.querySelector("#chat");
+            let textAreaMessage = document.querySelector("#textAreaMessage");
+            let sendBtn = document.querySelector("#sendBtn");
+            let title = document.querySelector("#title");
+            title.textContent = currentSubject.title;
+
+            for(let i=0;i<currentSubject.messages.length;i++)
             {
-                let message = 
-                {
-                    auteur: currentUser,
-                    message: text,
-                    date: new Date().toLocaleString()
-                };
-                addMessage(message);
-                textAreaMessage.value = "";
+                addMessage(currentSubject.messages[i]);
+            }
+        
+            textAreaMessage.style.height="1em";
+            textAreaMessage.style.height = "calc("+textAreaMessage.scrollHeight+"px - 1.245em)";
+            textAreaMessage.addEventListener("input", ()=>
+            {
                 textAreaMessage.style.height="1em";
-                textAreaMessage.style.height= textAreaMessage.scrollHeight+"px";
-                chat.scrollTop = chat.scrollHeight;
+                textAreaMessage.style.height = "calc("+textAreaMessage.scrollHeight+"px - 1.245em)";
+                textAreaMessage.scrollTop = textAreaMessage.scrollHeight;
+            });
+            //Redimensionne le textArea quand il change de taille (surtout quand il change de largeur).
+            function textAreaResize()
+            {
+                textAreaMessage.style.height="1em";
+                textAreaMessage.style.height = "calc("+textAreaMessage.scrollHeight+"px - 1.245em)";
+            }
+            new ResizeObserver(textAreaResize).observe(textAreaMessage);
+            textAreaMessage.addEventListener("keydown", (e)=>
+            {
+                if(e.key=="Enter" && !e.shiftKey)
+                {
+                    e.preventDefault();
+                    let text = textAreaMessage.value.trim();
+                    if(text)
+                    {
+                        let message = 
+                        {
+                            auteur: currentUser,
+                            message: text,
+                            date: new Date().toLocaleString()
+                        };
+                        addMessage(message);
+                        currentSubject.messages.push(message);
+                        currentSubject.lastComment = message.date;
+                        localStorage.setItem("categories", JSON.stringify(categories));
+                        textAreaMessage.value = "";
+                        textAreaMessage.style.height="1em";
+                        textAreaMessage.style.height= textAreaMessage.scrollHeight+"px";
+                        chat.scrollTop = chat.scrollHeight;
+                    }
+                }
+            });
+            sendBtn.addEventListener("click", ()=>
+            {
+                let text = textAreaMessage.value.trim();
+                if(text)
+                {
+                    let message = 
+                    {
+                        auteur: currentUser,
+                        message: text,
+                        date: new Date().toLocaleString()
+                    };
+                    addMessage(message);
+                    currentSubject.messages.push(message);
+                    currentSubject.lastComment = message.date;
+                    localStorage.setItem("categories", JSON.stringify(categories));
+                    textAreaMessage.value = "";
+                    textAreaMessage.style.height="1em";
+                    textAreaMessage.style.height= textAreaMessage.scrollHeight+"px";
+                    chat.scrollTop = chat.scrollHeight;
+                }
+            });
+
+            /**
+             * Crée le message sur la page html.
+             * @param {object} message
+             * @param {string} message.auteur
+             * @param {string} message.message
+             * @param {string} message.date
+             */
+            function addMessage(message)
+            {
+                let divMessage = chat.appendChild(document.createElement("div"));
+                divMessage.classList.add("message");
+                let auteurDiv = divMessage.appendChild(document.createElement("div"));
+                let auteurName = auteurDiv.appendChild(document.createElement("span"));
+                auteurName.style.fontSize = "150%";
+                auteurName.style.fontWeight = "bold";
+                auteurName.textContent = message.auteur.firstName + " " + message.auteur.lastName + " ";
+                auteurDiv.appendChild(document.createElement("span")).textContent = message.date;
+                //divMessage.appendChild(document.createElement("h2")).textContent = message.auteur.firstName + " " + message.auteur.lastName;
+                let span = divMessage.appendChild(document.createElement("span"));
+                span.textContent = message.message;
+                span.style.whiteSpace = "pre-wrap";
+                span.style.wordBreak = "break-word";
+                //divMessage.appendChild(document.createElement("h2")).textContent = message.date;
+                chat.append(document.createElement("hr"));
             }
         }
-    });
-
-    sendBtn.addEventListener("click", ()=>
-    {
-        let text = textAreaMessage.value.trim();
-        if(text)
-        {
-            let message = 
-            {
-                auteur: currentUser,
-                message: text,
-                date: new Date().toLocaleString()
-            };
-            addMessage(message);
-            textAreaMessage.value = "";
-            textAreaMessage.style.height="1em";
-            textAreaMessage.style.height= textAreaMessage.scrollHeight+"px";
-            chat.scrollTop = chat.scrollHeight;
-        }
-    });
-    
-    /**
-     * Crée le message sur la page html.
-     * @param {object} message
-     * @param {string} message.auteur
-     * @param {string} message.message
-     * @param {string} message.date
-     */
-    function addMessage(message)
-    {
-        let divMessage = chat.appendChild(document.createElement("div"));
-        divMessage.classList.add("message");
-        let auteurDiv = divMessage.appendChild(document.createElement("div"));
-        let auteurName = auteurDiv.appendChild(document.createElement("span"));
-        auteurName.style.fontSize = "150%";
-        auteurName.style.fontWeight = "bold";
-        auteurName.textContent = message.auteur.firstName + " " + message.auteur.lastName + " ";
-        auteurDiv.appendChild(document.createElement("span")).textContent = message.date;
-        //divMessage.appendChild(document.createElement("h2")).textContent = message.auteur.firstName + " " + message.auteur.lastName;
-        let span = divMessage.appendChild(document.createElement("span"));
-        span.textContent = message.message;
-        span.style.whiteSpace = "pre-wrap";
-        span.style.wordBreak = "break-word";
-        //divMessage.appendChild(document.createElement("h2")).textContent = message.date;
-        chat.append(document.createElement("hr"));
     }
 }
